25829bb835d57b68a534eea7e4667573cf42c1ad
Cristimiro Robert <cristimirorobert@gmail.com>
2023-05-08 08:11:18 -0300

    I added a header component with a logo and navigation menu and a footer component with copyright information. I also defined the colors that will be used throughout the application to ensure visual consistency.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,33 @@
+// React hooks
+import { useState,useEffect } from "react";
+
+// Third-party library
+import { ReactSVG } from "react-svg";
+
+// Images and icons
+import HeartIcon from '../../assets/svg/icon-heart.svg';
+
 export const Footer = () => {
+
+    const [currentDate, setCurrentDate] = useState<number>();
+
+    useEffect(() => {
+      setCurrentDate(new Date().getFullYear());
+    }, []);
+
   return (
-   <></>
+    <>
+      <footer>
+        <div className="container flex-between">
+          <p className="p1">
+            {currentDate} © <span></span> All rights Reserved
+          </p>
+          <div className="flex">
+            <ReactSVG src={HeartIcon} data-alt="Icon Heart" className="mr-1" />
+            <p className="p1">Designed and build by Cristimiro</p>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,107 @@
+// React hooks
+import {useRef, useEffect, useState } from 'react';
+
+// Third-party library
+import { ReactSVG } from 'react-svg';
+import { Link } from 'react-router-dom';
+
+// Images and icons
+import Logo from '../../assets/svg/logo.svg';
+
 export const Header = () => {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const headerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.pageYOffset;
+      if (currentScrollPosition > scrollPosition) {
+        document.getElementById('header')?.classList.add('header-active');
+      } else {
+        if (currentScrollPosition === 0) {
+          document.getElementById('header')?.classList.remove('header-active');
+        }
+      }
+      setScrollPosition(currentScrollPosition);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollPosition]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    const header = headerRef.current;
+    if (header) {
+      header.classList.toggle("header-active");
+    }
+  };
+
+
   return (
-    <></>
+    <>
+      <header id="header" className="header-menu " ref={headerRef}>
+        <nav className="container flex-between">
+          <Link to="/" className="logo" data-aos="fade-up" aria-label="Home">
+            <ReactSVG src={Logo} data-alt="Logo Portfolio" />
+          </Link>
+
+           <div  className={`menu-hamburger ${isMenuOpen ? 'menu-hamburger-active' : ''}`} onClick={toggleMenu}></div>
+          <ul className="menu-desktop flex">
+            <li data-aos="fade-up" data-aos-delay="300">
+              <a href="#services" className="nav-link-desktop" aria-label='Services'>
+                Services
+              </a>
+            </li>
+            <li data-aos="fade-up" data-aos-delay="600">
+              <a href="#portfolio" className="nav-link-desktop" aria-label='Portfolio'>
+                Portfolio
+              </a>
+            </li>
+            <li data-aos="fade-up" data-aos-delay="900">
+              <a href="#about" className="nav-link-desktop" aria-label='About Me'>
+                About Me
+              </a>
+            </li>
+            <li data-aos="fade-up" data-aos-delay="1200">
+              <Link to="/contact" className="nav-link-desktop" aria-label='Contact'>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="relative">
+        <nav className="nav-mobile">
+          <ul className={`menu-mobile ${isMenuOpen ? 'menu-mobile-active' : ''}`} onClick={toggleMenu}>
+            <li>
+              <a href="#services" className="nav-link-mobile" aria-label='Services'>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="nav-link-mobile" aria-label='Portfolio'>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#aboutMe" className="nav-link-mobile" aria-label='About Me'>
+                About Me
+              </a>
+            </li>
+            <li>
+              <Link to="/contact" className="nav-link-mobile" aria-label='Contact' >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
